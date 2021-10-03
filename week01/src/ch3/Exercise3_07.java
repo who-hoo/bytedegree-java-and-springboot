d69b383ad42da77d965c680f62a49e648b80d2c7
who-hoo <68011320+who-hoo@users.noreply.github.com>
2021-10-03 23:28:14 +0900

    Update: week01/src/ch3/Exercise3_07.java

--- week01/src/ch3/Exercise3_07.java
+++ week01/src/ch3/Exercise3_07.java
@@ -5,7 +5,8 @@ class Exercise3_07 {
 	public static void main(String[] args) {
 		int fahrenheit = 100;
 		float celcius = 5/9f * (fahrenheit - 32); // 37.77778
-		// TODO : Math.round()를 사용하지 않고 celcius 소수점 셋째자리에서 반올림하기
+		// Math.round()를 사용하지 않고 celcius 소수점 셋째자리에서 반올림하기
+		celcius = (int)((celcius)*100+0.5) / 100f;
 		
 		System.out.println("Fahrenheit: " + fahrenheit); // 100
 		System.out.println("Celcius: " + celcius); // 37.78
