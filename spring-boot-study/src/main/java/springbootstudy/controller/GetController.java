4d8d987f3d6c52b1bdfb78ec54e0ef1bbbe07540
who-hoo <who.ho3ov@gmail.com>
2021-11-08 10:30:06 +0900

    Feat: GET 예제 코드 작성

---
+++ spring-boot-study/src/main/java/springbootstudy/controller/GetController.java
@@ -0,0 +1,31 @@
+package springbootstudy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springbootstudy.model.SearchParam;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    // localhost:8080/api/get
+    @RequestMapping(method = RequestMethod.GET, path = "/get")
+    public String get() {
+        return "hello world";
+    }
+
+    // localhost:8080/api/getSingleParameter?name=hoo
+    @GetMapping("/getSingleParameter")
+    public String getSingleParameter(@RequestParam(name = "name") String param) {
+        return "request param : " + param;
+    }
+
+    // localhost:8080/api/getMultiParameter?account=hoo&email=multi@param.practice&phoneNumber=010-1234-5678
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam param) {
+        return param;
+    }
+}
