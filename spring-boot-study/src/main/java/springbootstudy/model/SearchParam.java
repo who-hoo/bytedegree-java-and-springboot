61272501da6eb3bb3bbaf2fdc20bec87ce0e4865
who-hoo <who.ho3ov@gmail.com>
2021-11-08 10:30:06 +0900

    Feat: GET 예제 코드 작성

---
+++ spring-boot-study/src/main/java/springbootstudy/model/SearchParam.java
@@ -0,0 +1,32 @@
+package springbootstudy.model;
+
+public class SearchParam {
+
+    private String account;
+    private String email;
+    private String phoneNumber;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
