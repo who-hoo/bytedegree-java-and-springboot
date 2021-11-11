8c8ab8ccf281b487e71c90c617a6b8a36d762348
who-hoo <who.ho3ov@gmail.com>
2021-11-11 23:16:58 +0900

    Feat: OrderDetail CRUD 작성

---
+++ spring-boot-study/src/test/java/springbootstudy/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,61 @@
+package springbootstudy.repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import springbootstudy.SpringBootStudyApplicationTests;
+import springbootstudy.model.entity.OrderDetail;
+
+public class OrderDetailRepositoryTest extends SpringBootStudyApplicationTests {
+
+    @Autowired
+    OrderDetailRepository orderDetailRepository;
+
+    @Test
+    @Transactional
+    public void create() {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setOrderAt(LocalDateTime.now());
+        orderDetail.setUserId(7L);
+        orderDetail.setItemId(1L);
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+        System.out.println("newOrderDetail : " + newOrderDetail);
+    }
+
+    @Test
+    public void read() {
+        Optional<OrderDetail> orderDetail = orderDetailRepository.findById(1L);
+        orderDetail.ifPresent(selectedOrderDetail -> {
+            System.out.println("OrderDetail : " + selectedOrderDetail);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void update() {
+        Optional<OrderDetail> orderDetail = orderDetailRepository.findById(1L);
+        orderDetail.ifPresent(selectedOrderDetail -> {
+            selectedOrderDetail.setOrderAt(LocalDateTime.now());
+
+            orderDetailRepository.save(selectedOrderDetail);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void delete() {
+        Optional<OrderDetail> orderDetail = orderDetailRepository.findById(1L);
+        orderDetail.ifPresent(selectedOrderDetail -> {
+            orderDetailRepository.delete(selectedOrderDetail);
+        });
+        Optional<OrderDetail> deletedOrderDetail = orderDetailRepository.findById(1L);
+        if (deletedOrderDetail.isPresent()) {
+            System.out.println("데이터 삭제 실패 : " + deletedOrderDetail.get());
+        } else {
+            System.out.println("데이터 삭제 성공");
+        }
+    }
+}
