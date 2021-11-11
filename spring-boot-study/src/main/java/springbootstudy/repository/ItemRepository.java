5c75f31031e97e0580e4c460e3a518697c1074ea
who-hoo <who.ho3ov@gmail.com>
2021-11-11 21:05:13 +0900

    Feat: ItemRepository 생성

---
+++ spring-boot-study/src/main/java/springbootstudy/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package springbootstudy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import springbootstudy.model.entity.Item;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
