29aedd047b9dac4aa60bf7bc6ba7959694f142fe
who-hoo <who.ho3ov@gmail.com>
2021-11-12 00:02:56 +0900

    Feat: QueryMethod 예제 코드 작성

--- spring-boot-study/src/main/java/springbootstudy/repository/UserRepository.java
+++ spring-boot-study/src/main/java/springbootstudy/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package springbootstudy.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import springbootstudy.model.entity.User;
@@ -7,4 +8,9 @@ import springbootstudy.model.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByAccount(String account);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByAccountAndEmail(String account, String email);
 }
