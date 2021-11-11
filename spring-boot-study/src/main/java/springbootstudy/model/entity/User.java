2ec5c7a0abe6719a4b3a5b8bac3cfc3d1bd0eb77
who-hoo <who.ho3ov@gmail.com>
2021-11-11 10:51:14 +0900

    Feat: User Entity 생성

---
+++ spring-boot-study/src/main/java/springbootstudy/model/entity/User.java
@@ -0,0 +1,29 @@
+package springbootstudy.model.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
