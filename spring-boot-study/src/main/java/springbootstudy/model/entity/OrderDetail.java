5707d2a4978d9e168ff910b415bd6345dfc0631a
who-hoo <who.ho3ov@gmail.com>
2021-11-11 21:03:17 +0900

    Feat: OrderDetail Entity 생성

---
+++ spring-boot-study/src/main/java/springbootstudy/model/entity/OrderDetail.java
@@ -0,0 +1,22 @@
+package springbootstudy.model.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime orderAt;
+
+    private Long userId;
+
+    private Long itemId;
+}
