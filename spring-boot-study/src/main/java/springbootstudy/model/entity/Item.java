b254380bd53bf120b4f169f9670fb394b7808a40
who-hoo <who.ho3ov@gmail.com>
2021-11-11 17:42:15 +0900

    Feat: Item Entity 생성

---
+++ spring-boot-study/src/main/java/springbootstudy/model/entity/Item.java
@@ -0,0 +1,21 @@
+package springbootstudy.model.entity;
+
+import javax.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer price;
+
+    private String content;
+}
