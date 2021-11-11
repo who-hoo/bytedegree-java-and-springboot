9553e0a668c9a422015e279547cdca10c2e00f21
who-hoo <who.ho3ov@gmail.com>
2021-11-11 21:04:42 +0900

    Fix: UserRepository JpaRepository 구현하도록 수정

--- spring-boot-study/src/main/java/springbootstudy/repository/UserRepository.java
+++ spring-boot-study/src/main/java/springbootstudy/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package springbootstudy.repository;
 
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import springbootstudy.model.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepositoryImplementation<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
