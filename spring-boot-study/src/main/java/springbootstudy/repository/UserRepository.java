85ac9ac46ff88131d6bdeb1af1591d96253bffbc
who-hoo <who.ho3ov@gmail.com>
2021-11-15 21:11:41 +0900

    Feat: User - create, read 작성

--- spring-boot-study/src/main/java/springbootstudy/repository/UserRepository.java
+++ spring-boot-study/src/main/java/springbootstudy/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package springbootstudy.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import springbootstudy.model.entity.User;
@@ -8,9 +7,5 @@ import springbootstudy.model.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByAccount(String account);
-
-    Optional<User> findByEmail(String email);
-
-    Optional<User> findByAccountAndEmail(String account, String email);
+    User findFirstByPhoneNumberOrderByIdDesc(String phoneNumber);
 }
