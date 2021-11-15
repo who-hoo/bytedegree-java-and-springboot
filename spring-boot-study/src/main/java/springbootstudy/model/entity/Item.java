28f9f857d4fd26eb93887f4a58333a665108f902
who-hoo <who.ho3ov@gmail.com>
2021-11-16 00:39:39 +0900

    Feat: AuditingEntityListener 추가

--- spring-boot-study/src/main/java/springbootstudy/model/entity/Item.java
+++ spring-boot-study/src/main/java/springbootstudy/model/entity/Item.java
@@ -4,11 +4,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @ToString(exclude = {"partner", "orderDetailList"})
 public class Item {
 
@@ -32,12 +38,16 @@ public class Item {
 
     private LocalDateTime unregisteredAt;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @CreatedBy
     private String createdBy;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     private String updatedBy;
 
     @ManyToOne
--- spring-boot-study/src/main/java/springbootstudy/model/entity/User.java
+++ spring-boot-study/src/main/java/springbootstudy/model/entity/User.java
@@ -4,11 +4,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @ToString(exclude = {"orderGroupList"})
 public class User {
 
@@ -30,12 +36,16 @@ public class User {
 
     private LocalDateTime unregisteredAt;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @CreatedBy
     private String createdBy;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     private String updatedBy;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
