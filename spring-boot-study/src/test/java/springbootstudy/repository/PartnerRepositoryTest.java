2bbbd5493cb75090c3b51c1b394705bd6d02b9ee
who-hoo <who.ho3ov@gmail.com>
2021-11-15 21:35:36 +0900

    Feat: Partner - create 작성

---
+++ spring-boot-study/src/test/java/springbootstudy/repository/PartnerRepositoryTest.java
@@ -0,0 +1,33 @@
+package springbootstudy.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import springbootstudy.SpringBootStudyApplicationTests;
+import springbootstudy.model.entity.Partner;
+
+public class PartnerRepositoryTest extends SpringBootStudyApplicationTests {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Test
+    public void create() {
+        Partner partner = new Partner();
+        partner.setName("Partner01");
+        partner.setStatus("REGISTERED");
+        partner.setAddress("서울시 강남구");
+        partner.setCallCenter("010-1234-5678");
+        partner.setBusinessNumber("1234567890123");
+        partner.setCeoName("홍길동");
+        partner.setRegisteredAt(LocalDateTime.now());
+        partner.setCreatedAt(LocalDateTime.now());
+        partner.setCreatedBy("admin");
+        partner.setCategoryId(1L);
+
+        Partner newPartner = partnerRepository.save(partner);
+        assertNotNull(partner);
+    }
+}
