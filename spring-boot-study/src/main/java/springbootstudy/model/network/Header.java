eeabb69d5f0e95cdf36c0920497f78eee4da2f47
who-hoo <who.ho3ov@gmail.com>
2021-11-16 12:06:46 +0900

    Feat: Response Header 생성 함수 작성

--- spring-boot-study/src/main/java/springbootstudy/model/network/Header.java
+++ spring-boot-study/src/main/java/springbootstudy/model/network/Header.java
@@ -1,5 +1,6 @@
 package springbootstudy.model.network;
 
+import java.time.LocalDateTime;
 import lombok.*;
 
 @Data
@@ -8,11 +9,36 @@ import lombok.*;
 @Builder
 public class Header<T> {
 
-    private String transactionTime;
+    private LocalDateTime transactionTime;
 
     private String resultCode;
 
     private String description;
 
     private T data;
+
+    public static <T> Header<T> OK() {
+        return (Header<T>) Header.builder()
+            .transactionTime(LocalDateTime.now())
+            .resultCode("OK")
+            .description("OK")
+            .build();
+    }
+
+    public static <T> Header<T> OK(T data) {
+        return (Header<T>) Header.builder()
+            .transactionTime(LocalDateTime.now())
+            .resultCode("OK")
+            .description("OK")
+            .data(data)
+            .build();
+    }
+
+    public static <T> Header<T> ERROR(String description) {
+        return (Header<T>) Header.builder()
+            .transactionTime(LocalDateTime.now())
+            .resultCode("ERROR")
+            .description(description)
+            .build();
+    }
 }
