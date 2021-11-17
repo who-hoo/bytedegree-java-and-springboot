96b07d526d548df835d842204fd5e8e5048755dc
who-hoo <who.ho3ov@gmail.com>
2021-11-17 22:01:59 +0900

    Feat: User API - create 작성

---
+++ spring-boot-study/src/main/java/springbootstudy/controller/api/UserApiController.java
@@ -0,0 +1,44 @@
+package springbootstudy.controller.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.common.util.impl.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import springbootstudy.interfaces.CrudInterface;
+import springbootstudy.model.network.Header;
+import springbootstudy.model.network.request.UserApiRequest;
+import springbootstudy.model.network.response.UserApiResponse;
+import springbootstudy.service.UserApiLogicService;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserApiLogicService userApiLogicService;
+
+    @Override
+    @PostMapping("")
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        return userApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header<UserApiResponse> delete(@PathVariable(name = "id") Long id) {
+        return null;
+    }
+}
