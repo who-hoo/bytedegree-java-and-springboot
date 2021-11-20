3d4ca5cfc917f1e0685da4a31acd98c96b53cc4c
who-hoo <who.ho3ov@gmail.com>
2021-11-21 00:51:53 +0900

    Feat: getPrice 메서드 작성

--- faststore-assignment/src/main/java/com/fastcampus/faststore/entity/BookSale.java
+++ faststore-assignment/src/main/java/com/fastcampus/faststore/entity/BookSale.java
@@ -19,14 +19,14 @@ import javax.persistence.ManyToOne;
  * 책과 할인 정책 정보를 함께 관리한다
  * */
 public class BookSale extends BaseEntity {
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private Book book;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = true)
     private DiscountPolicy discountPolicy;
 
-    // TODO: 책이 본래 가진 price에 할인 정책을 적용하여 가격을 반환하도록 한다. BookSaleTest를 성공시킨다.
     public Long getPrice() {
-        return 0L;
+        return discountPolicy.getDiscountAmount(book.getPrice());
     }
 }
